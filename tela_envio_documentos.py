1bb6c9e0900bdf495863df5f17b793ee63b7aa23
jaque-linee <jkellyne@gmail.com>
2026-08-29 12:37:47 -0300

    Update tela_envio_documentos.py

--- tela_envio_documentos.py
+++ tela_envio_documentos.py
@@ -169,7 +169,8 @@ def exibir_tela_envio_documentos(
                     dados = extrair_dados(
                         texto,
                         itens,
-                        tipo
+                        tipo,
+                        nome_arquivo=arquivo.name
                     )
 
                     duplicado, existente = verificar_duplicidade(
